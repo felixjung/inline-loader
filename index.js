21de2ece7e882b3174d5ab395636a5308d80ce44
Mikael Lambert <michael.r.lam@gmail.com>
2016-07-19 07:34:50 -0600

    adding relativeTo

--- index.js
+++ index.js
@@ -10,8 +10,42 @@ module.exports = function(content) {
   this.cacheable && this.cacheable();
 
   // Parse query into options and add resourcePath to options
-  var options = utils.parseQuery(this.query);
-  options.resourcePath = this.resourcePath;
+  var query = utils.parseQuery(this.query);
+  var options = {
+      resourcePath: this.resourcePath
+  };
+
+  // relativeTo from https://github.com/WearyMonkey/ngtemplate-loader
+  var relativeTo = query.relativeTo || '';
+  var absolute = false;
+  var pathSep = query.pathSep || '/';
+  var resource = this.resource;
+  var pathSepRegex = new RegExp(escapeRegExp(path.sep), 'g');
+
+  // if a unix path starts with // we treat is as an absolute path e.g. //Users/wearymonkey
+  // if we're on windows, then we ignore the / prefix as windows absolute paths are unique anyway e.g. C:\Users\wearymonkey
+  if (relativeTo[0] == '/') {
+      if (path.sep == '\\') { // we're on windows
+          relativeTo = relativeTo.substring(1);
+      } else if (relativeTo[1] == '/') {
+          absolute = true;
+          relativeTo = relativeTo.substring(1);
+      }
+  }
+
+  // normalise the path separators
+  if (path.sep != pathSep) {
+      relativeTo = relativeTo.replace(pathSepRegex, pathSep);
+      resource = resource.replace(pathSepRegex, pathSep)
+  }
+
+  var relativeToIndex = resource.indexOf(relativeTo);
+  if (relativeToIndex === -1 || (absolute && relativeToIndex !== 0)) {
+      throw 'The path for file doesn\'t contain relativeTo param';
+  }
+
+  // reassign resource path
+  options.resourcePath = resource.slice(relativeToIndex + relativeTo.length); // get the base path
 
   var loaderCode = [
     '// inline-loader: inserts the content of a resource into the DOM.',
@@ -26,4 +60,9 @@ module.exports = function(content) {
   ].join('\n');
 
   return loaderCode;
+
+  // source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
+  function escapeRegExp(string) {
+      return string.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+  }
 };
